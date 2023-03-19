70136d1096d4c8e19f76e625b33ef7d76787b232
DarkOnion0 <darkonion0@clapot.org>
2023-03-19 12:56:04 +0100

    feat: init project

---
+++ cli.fish
@@ -0,0 +1,46 @@
+#!/usr/bin/env fish
+
+# Define the available CMD flag
+set --local options 'h/help' 'f/file=' 's/style=' 'o/orientation='
+
+# Parse the CMD flag
+argparse $options -- $argv
+
+# Take action according to the provdided CMD flags
+
+if set --query _flag_help
+  printf "Usage: [OPTIONS]\n\n"
+  printf "Options:\n"
+  printf "  -h/--help                       Prints help and exits\n"
+  printf "  -f/--file                       Get a markdown file to convert to PDF\n"
+  printf "  -s/--exatra-style               Set some style overrides, /!\\ Be aware that this will make the style less reproductible /!\\ \n"
+  printf "  -o/--orientatation-portrait     Set the output pdf orientation, (portrait)/landscape\n"
+  return 0
+end
+
+if set --query _flag_file
+    #set should_run "true"
+    
+    printf "%s\n" $_flag_file
+    #cat $_flag_file
+else
+    set _flag_file "./texte.md"
+end 
+
+if set --query _flag_style
+    #set should_run "true"
+    printf "%s\n" $_flag_style
+end
+
+if set --query _flag_orientation
+    #set should_run "true"
+    printf "%s\n" $_flag_orientation
+else
+    #set should_run "true"
+    set _flag_orientation "portrait"
+    #printf "%s\n" $_flag_orientation
+end
+
+# Run the parser
+
+pandoc $_flag_file -t html --pdf-engine weasyprint --css @styleDir@/css/style-$_flag_orientation.css --template @templateDir@/default.html --katex -o (printf $_flag_file | sed 's/.md/.pdf/g')
---
+++ flake.nix
@@ -0,0 +1,137 @@
+{
+  description = "A CLI app to render markdown code to awesome PDF with the help of pandoc";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/22.11";
+    flake-utils.url = "github:numtide/flake-utils";
+    devenv.url = "github:cachix/devenv/latest";
+  };
+
+  outputs = inputs @ {
+    self,
+    flake-utils,
+    nixpkgs,
+    devenv,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        inherit (builtins) substring;
+
+        # to work with older version of flakes
+        lastModifiedDate = self.lastModifiedDate or self.lastModified or "19700101";
+
+        # Generate a user-friendly version number.
+        version = builtins.substring 0 8 lastModifiedDate;
+
+        # Packages aliases
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        prodPackages = with pkgs; [
+          fish
+          python3Packages.weasyprint
+          #wkhtmltopdf
+          nerdfonts
+          pandoc
+          nodePackages.sass
+        ];
+      in rec {
+        devShell = devenv.lib.mkShell {
+          inherit inputs pkgs;
+          modules = [
+            {
+              enterShell = ''
+                # Welcome script
+                echo -e "\n$(tput bold)Welcome in the nix flake of PandocTheming repo$(tput sgr0)"
+              '';
+
+              packages = with pkgs;
+                [
+                  alejandra
+                  nodePackages.prettier
+                ]
+                ++ prodPackages;
+
+              scripts = {
+                run-build.exec = "sass --update --style compressed 'style/sass:style/css'";
+              };
+
+              pre-commit.hooks = {
+                # Nix
+                alejandra.enable = true;
+
+                # Markdown...
+                prettier = {
+                  enable = true;
+                  files = "\\.(scss|md|html)$";
+                };
+
+                # Sass compilation
+                #sass-compilation = {
+                #  enable = true;
+                #  name = "Sass Compilation";
+                #  entry = "run-build";
+                #  files = "\\.scss$";
+                #};
+              };
+            }
+          ];
+        };
+
+        packages = rec {
+          default = pandocTheming;
+          pandocTheming = pkgs.stdenv.mkDerivation {
+            inherit version;
+            name = "pandocTheming";
+            src = ./.;
+
+            dontUnpack = true;
+
+            buildInputs = prodPackages;
+
+            nativeBuildInputs = [
+              pkgs.makeWrapper
+            ];
+
+            installPhase = ''
+              runHook preInstall
+
+              mkdir -p $out/bin
+
+              ls -larth $src/style
+
+              #sass $src/style/sass:$src/style/css # TODO: implement this inside nix
+              cp -r $src/style $out/
+              cp -r $src/template $out/
+              cp -r $src/cli.fish $out/
+              cp -r $src/filter $out/
+
+              chmod +x $out/cli.fish
+
+              runHook postInstall
+            '';
+
+            preFixup = ''
+              # Replace var inside cli
+              # TODO: use substitute-all instead https://github.com/NixOS/nixpkgs/blob/master/pkgs/build-support/substitute/substitute-all.nix
+              sed -i "s|@styleDir@|$out/style|g" $out/cli.fish
+              sed -i "s|@templateDir@|$out/template|g" $out/cli.fish
+
+              cat $out/cli.fish
+
+              makeWrapper $out/cli.fish $out/bin/pandocTheming \
+                --prefix PATH : ${pkgs.lib.makeBinPath prodPackages} \
+            '';
+          };
+        };
+
+        apps = rec {
+          default = pandocTheming;
+          pandocTheming = {
+            type = "app";
+            program = "${self.packages.${system}.default}/bin/pandocTheming";
+          };
+        };
+      }
+    );
+}
